79073450e18d0627f8ba459b1d87a6904df39661
ThAnthony <jair.guillen2001@gmail.com>
2025-03-20 19:47:32 -0500

    login funcional

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  async onSubmit() {
+    console.log("onSubmit");
+    const response = await this.authService.login(this.username, this.password);
+    if (response.isLogin) {
+      console.log("logueado");
+      this.router.navigate(['/home']);
+    }
+    else {
+      console.log("No logueado");
+      this.errorMessage = 'Invalid username or password';
+    }
+
+    if (response.user) {
+    console.log("role:",response.user.rol);  
+    }
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { firstValueFrom, map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) {}
+
+  async login(username: string, password: string): Promise<any> {
+    const url = `${this.apiUrl}/user/login`;
+    const response = await firstValueFrom(
+      this.http.post<{user?:any, isLogin:string}>(
+        url, { username, password }));
+    console.log("Servicio auth.user:",response.user);
+    console.log("Servicio auth-message:",response.isLogin);
+    return response;
+  }
+
+}
